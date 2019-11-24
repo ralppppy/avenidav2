15ab1237d5b447aa6a671d8d68db5c810b55ed00
Ralp Yosores <ralpyosores@gmail.com>
2019-11-24 09:53:02 +0800

    Updated

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7bd24f4506e5b9378429a75fe8e2063a.js"
+  "/precache-manifest.74f65536789bb46b271fb0d892cdb071.js"
 );
 
 self.addEventListener('message', (event) => {
